2438d2eea7464c2ed9784ac5fe52bebfd2d5b75c
PacMan144 <cutedumplinguwu0@gmail.com>
2025-03-25 16:14:31 +0200

    Create shitty.js

---
+++ shitty.js
@@ -0,0 +1,28 @@
+  ingredients.cum = {
+    type: "liquid",
+    color: [ "#D6D6CB" ],
+    keywords: 'semen,sperm,jizz,spunk,nut,baby batter,cream',
+    dishWeight: -55,
+    adj: "cummy",
+    "shape": "liquid"
+  };
+
+  ingredients.poop = {
+    shape:"blob",
+    type:"paste",
+    color:["#5C3508"],
+    adj:"toxic",
+    hidden:true,
+    keywords: 'doo,poo,shit,crap,feces,scat,dung,number 2,#2'
+  }
+}
+
+addRecipe("doll+baguette","kasane teto");
+  }
+}
+
+addIngredient("pink_dye",{
+    type:"red_dye",
+    color:"#FFA6FF",
+    adj:"magenta",
+});
